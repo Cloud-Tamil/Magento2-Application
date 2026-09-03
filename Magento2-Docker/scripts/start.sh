1e64647f323a06fe3b104376bc65d1d21538c20c
Cloud Engineer <sktamilvb@gmail.com>
2026-09-03 16:30:14 +0530

    Update start.sh

--- Magento2-Docker/scripts/start.sh
+++ Magento2-Docker/scripts/start.sh
@@ -4,6 +4,35 @@ set -Eeuo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
+
+echo
+echo "=================================================="
+echo " Starting Magento Docker"
+echo "=================================================="
+echo
+
+
 docker compose up -d
 
+
+echo
+echo "Waiting for containers..."
+sleep 5
+
+
 docker compose ps
+
+
+echo
+echo "Magento:"
+echo "http://localhost:${VARNISH_PORT:-8080}"
+
+echo
+echo "Admin:"
+echo "http://localhost:${VARNISH_PORT:-8080}/admin"
+
+echo
+echo "Direct Nginx:"
+echo "http://localhost:${NGINX_PORT:-8081}"
+
+echo
