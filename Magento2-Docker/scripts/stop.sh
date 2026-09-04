57012b39f32245f338e9e662648433abbc8b37bb
Cloud Engineer <sktamilvb@gmail.com>
2026-09-04 13:31:57 +0530

    Update stop.sh

--- Magento2-Docker/scripts/stop.sh
+++ Magento2-Docker/scripts/stop.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+
 set -Eeuo pipefail
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
 
 echo
 echo "Stopping Magento Docker..."
 echo
+
+
 docker compose stop
+
 
 echo
 echo "Magento Docker stack stopped."
