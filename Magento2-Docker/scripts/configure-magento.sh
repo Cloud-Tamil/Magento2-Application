1a0d1957264fbc1ef19b2dc1a901971e9cffaaca
Cloud Engineer <sktamilvb@gmail.com>
2026-09-04 09:54:07 +0530

    Update configure-magento.sh

--- Magento2-Docker/scripts/configure-magento.sh
+++ Magento2-Docker/scripts/configure-magento.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-
 set -Eeuo pipefail
-
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-
 
 echo
 echo "=================================================="
@@ -11,44 +8,32 @@ echo " Magento Configuration"
 echo "=================================================="
 echo
 
-
 echo "[1] Magento upgrade"
-
 docker compose exec -T php \
     php bin/magento setup:upgrade
 
-
 echo
 echo "[2] Dependency compilation"
-
 docker compose exec -T php \
     php bin/magento setup:di:compile
 
-
 echo
 echo "[3] Static content"
-
 docker compose exec -T php \
     php bin/magento setup:static-content:deploy -f en_US
 
-
 echo
 echo "[4] Reindex"
-
 docker compose exec -T php \
     php bin/magento indexer:reindex
 
-
 echo
 echo "[5] Cache"
-
 docker compose exec -T php \
     php bin/magento cache:flush
 
-
 echo
 echo "[6] Permissions"
-
 docker compose exec -T php \
     chown -R www-data:www-data \
     var \
@@ -56,7 +41,6 @@ docker compose exec -T php \
     pub/static \
     pub/media \
     app/etc
-
 
 docker compose exec -T php \
     chmod -R ug+rwX \
@@ -66,12 +50,9 @@ docker compose exec -T php \
     pub/media \
     app/etc
 
-
 echo
 echo "Restarting web services..."
-
 docker compose restart nginx varnish
-
 
 echo
 echo "Magento configuration completed."
