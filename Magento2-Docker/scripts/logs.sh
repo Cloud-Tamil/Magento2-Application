11a4350d7bfe0d1464ecb7509c3afb3deb2d2be1
Cloud Engineer <sktamilvb@gmail.com>
2026-09-04 13:30:55 +0530

    Update logs.sh

--- Magento2-Docker/scripts/logs.sh
+++ Magento2-Docker/scripts/logs.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
+
 set -Eeuo pipefail
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
 
 SERVICE="${1:-}"
 
-if [[ -n "$SERVICE" ]]; then
-    docker compose logs -f "$SERVICE"
+
+if [[ -n "${SERVICE}" ]]
+then
+
+    docker compose logs -f "${SERVICE}"
+
 else
+
     docker compose logs -f
+
 fi
