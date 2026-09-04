3fb2196e4e2253442be75afae204041588fdaa62
Cloud Engineer <sktamilvb@gmail.com>
2026-09-04 09:54:24 +0530

    Update generate-varnish-vcl.sh

--- Magento2-Docker/scripts/generate-varnish-vcl.sh
+++ Magento2-Docker/scripts/generate-varnish-vcl.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-
 set -Eeuo pipefail
-
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-
 
 echo
 echo "=================================================="
@@ -11,23 +8,16 @@ echo " Magento Varnish VCL Generator"
 echo "=================================================="
 echo
 
-
 if [[ ! -f src/app/etc/env.php ]]; then
-
     echo "ERROR: Magento is not installed."
-
     exit 1
 fi
 
-
 echo "Generating Magento Varnish configuration..."
-
-
 docker compose exec -T php \
     php bin/magento varnish:vcl:generate \
     --export-version=8 \
     > docker/varnish/generated.vcl
-
 
 echo
 echo "Generated:"
@@ -35,21 +25,14 @@ echo
 echo "docker/varnish/generated.vcl"
 echo
 
-
 echo "Applying generated VCL..."
-
-
 cp \
     docker/varnish/generated.vcl \
     docker/varnish/default.vcl
 
-
 echo
 echo "Restarting Varnish..."
-
-
 docker compose restart varnish
-
 
 echo
 echo "Varnish configuration updated."
