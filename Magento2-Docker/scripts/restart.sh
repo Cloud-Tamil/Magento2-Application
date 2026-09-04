da51d7e6b75548f27648eef35e6324d246427a78
Cloud Engineer <sktamilvb@gmail.com>
2026-09-04 09:56:17 +0530

    Update restart.sh

--- Magento2-Docker/scripts/restart.sh
+++ Magento2-Docker/scripts/restart.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
-
 set -Eeuo pipefail
-
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-
 
 echo
 echo "Restarting Magento Docker..."
 echo
-
-
 docker compose restart
-
 
 echo
 echo "Waiting..."
 sleep 5
-
-
 docker compose ps
-
 
 echo
 echo "Store:"
 echo "http://localhost:${VARNISH_PORT:-8080}"
-
 echo
